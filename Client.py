51344b0d06f453ea8b17084de26f3f220b8c2d92
tegnoue <tegnoue@outlook.com>
2026-08-30 19:57:16 -0300

    feat: server threading

--- Client.py
+++ Client.py
@@ -1,8 +1,12 @@
 import socket
 
 socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socketTCP.connect(("127.0.0.1", 2023))
-socketTCP.sendall(b"Mensagem.")
-msg = socketTCP.recv(1024)
-print(f"{msg} <- recebida")
-socketTCP.close()
+socketTCP.connect(("127.0.0.1", 2026))
+
+while True:
+
+    txt_message = input("Digite a sua mensagem:\n")
+    bytes_message = (txt_message).encode("ascii")
+    socketTCP.sendall(bytes_message)
+    msg = socketTCP.recv(1024)
+    print(f"{msg} <- recebida")
--- TCP_Server.py
+++ TCP_Server.py
@@ -1,15 +1,22 @@
 import socket
+import threading
+
+def establish_connection(clientAddress, conn):
+    print(f"conexao estabelecida com o cliente {clientAddress}")
+    while True:
+        msg = socketClient.recv(1024)
+        print(f"mensagem recebida: {msg}")
+        socketClient.sendall(str.encode("Mensagem recebida"))
+
+
+
 
 socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socketTCP.bind(("127.0.0.1", 2023))
+socketTCP.bind(("127.0.0.1", 2026))
+socketTCP.listen()
+
 while True:
-    socketTCP.listen()
-
-    socketCliente, endereco = socketTCP.accept()
-
-    msg = socketCliente.recv(1024)
-    print(f"mensagem recebida: {msg}")
-
-    socketCliente.sendall(str.encode("Mensagem recebida!"))
-    socketCliente.close()
+    socketClient, address = socketTCP.accept()
+    threadClient = threading.Thread(target=establish_connection, args=(socketClient, address))
+    threadClient.start()
